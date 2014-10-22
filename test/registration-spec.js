eda7f6f56b60a210d9d4cfbd90f5b288a89ab98a
Ruairi O Brien <ruairitobrien@gmail.com>
2014-10-22 23:19:56 +0100

    Updated team stuff

--- test/registration-spec.js
+++ test/registration-spec.js
@@ -70,9 +70,11 @@ describe('Registration', function () {
             regResult.user.organisations[0].should.eql(regResult.organisation._id);
         });
         it('user is owner of organisation', function () {
-            regResult.user.teams.length.should.equal(1);
+            regResult.user.teams.length.should.equal(2);
             should.exist(regResult.organisation.owners);
+            should.exist(regResult.organisation.members);
             regResult.user.teams[0].should.eql(regResult.organisation.owners);
+            regResult.user.teams[1].should.eql(regResult.organisation.members);
         });
         it('organisation has team called everyone by default', function () {
            should.exist(regResult.organisation.members);
